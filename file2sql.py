6883c84cd91e12ddaa15740aedefe9fea51e090d
Filipe Pinto Santos <filipepintosantos@gmail.com>
2017-05-28 11:23:59 +0100

    Work in progress - reads corrects and saves file.

--- file2sql.py
+++ file2sql.py
@@ -3,9 +3,9 @@
 """
 Programa para ler um ficheiro de texto e carregar numa tabela linha a linha
 
-Ler o ficheiro no arg1 linha-a-linha
-Remover carateres perigosos
-Voltar a gravar o ficheiro
+OK Ler o ficheiro no arg1 linha-a-linha
+OK Remover carateres perigosos
+OK Voltar a gravar o ficheiro
 Ler ficheiro de parametros para ligação ao SQL
 Integrar ficheiro no SQL ou
 Executar SQLCMD para integrar o ficheiro
@@ -13,7 +13,7 @@ Executar SQLCMD para integrar o ficheiro
 @Author: Filipe Santos
 @Created: 2017-05-27
 
-@Version: 0.1 b
+@Version: 0.2 b
 @Copyright: PSC - Pinto Santos Consultores - 2017
 
 """
@@ -21,8 +21,26 @@ Executar SQLCMD para integrar o ficheiro
 import sys
 #import subprocess
 
+#
+# TEST AREA
+#
+
+#
+# END OF TEST AREA
+#
+
+sqltable = "TABELA"
+sqlcolumn = "COLUNA"
+
 if len(sys.argv) > 1:
-    originalfile = sys.argv[1]
-    print(originalfile)
+    original_file = sys.argv[1]
+    tempfile = open("tempfile.sql", 'w')
+    file_lines = open(original_file).read().splitlines()
+    with open("tempfile.sql", 'w') as tempfile:
+        for idx, file_line in enumerate(file_lines):
+            sqlvalue = file_line.replace("'", " ")
+            new_file_line="insert into %s (%s) values ('%s');\n" % (sqltable, sqlcolumn, sqlvalue)
+            tempfile.write(new_file_line)
+
 else:
     print("argument 1 is empty.")
