41903c21b5901371087d15e349a93085b24d14a4
Filipe Pinto Santos <filipepintosantos@gmail.com>
2017-05-28 02:28:55 +0100

    Pequeno update ao TODO

--- file2sql.py
+++ file2sql.py
@@ -1,21 +1,28 @@
 # file2sql
 
 """
- Programa para ler um ficheiro de texto e carregar numa tabela linha a linha
+Programa para ler um ficheiro de texto e carregar numa tabela linha a linha
 
- Ler o ficheiro no arg1 linha-a-linha
- Remover carateres perigosos
- Voltar a gravar o ficheiro ou inserir no SQL
+Ler o ficheiro no arg1 linha-a-linha
+Remover carateres perigosos
+Voltar a gravar o ficheiro
+Ler ficheiro de parametros para ligação ao SQL
+Integrar ficheiro no SQL ou
+Executar SQLCMD para integrar o ficheiro
 
- @Author: Filipe Santos
- @Created: 2017-05-27
+@Author: Filipe Santos
+@Created: 2017-05-27
+
+@Version: 0.1 b
+@Copyright: PSC - Pinto Santos Consultores - 2017
 
 """
 
 import sys
+#import subprocess
 
 if len(sys.argv) > 1:
-    filename = sys.argv[1]
-    print (filename)
+    originalfile = sys.argv[1]
+    print(originalfile)
 else:
-    print ("argument 1 is empty.")
+    print("argument 1 is empty.")
